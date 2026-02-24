fed741245faef0841bcb2e56cc62c4f84678d4b5
Silvio Ernesto <silvioebis2005@gmail.com>
2026-02-24 15:16:16 -0300

    refactor: separate security data into UserSecurityProfile model

--- base/models.py
+++ base/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 #DOCUMENTATION ABOUT ID'S
 import uuid
@@ -66,6 +68,8 @@ class BaseCustomUser(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_login_ip = models.GenericIPAddressField(null=True, blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name']
 
     def is_manager_or_admin(self):
         return self.user_type in ['manager', 'admin'] or self.is_superuser
@@ -78,6 +82,28 @@ class BaseCustomUser(AbstractUser):
             self.is_superuser = True
         super().save(*args, **kwargs)
 
+class UserSecurityProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='security_profile')
+    is_2fa_enabled = models.BooleanField(default=False)
+    otp_code = models.CharField(max_length=6, blank=True, null=True)
+    otp_created_at = models.DateTimeField(blank=True, null=True)
+    reset_password_token = models.CharField(max_length=100, blank=True, null=True)
+    reset_password_expires = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Security Profile for {self.user.email}"
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_security_profile(sender, instance, created, **kwargs):
+    if created:
+        UserSecurityProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_security_profile(sender, instance, **kwargs):
+    if not hasattr(instance, 'security_profile'):
+        UserSecurityProfile.objects.create(user=instance)
+    else:
+        instance.security_profile.save()
 
 # ORDERS 
 
